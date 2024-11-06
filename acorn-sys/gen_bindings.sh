bbce02e324496f88386cd6f95291ecf0bda93814
Cedric Sirianni <cedric@sirianni.dev>
2024-11-06 11:22:09 -0500

    Generate FAISS bindings from ACORN fork
    Co-authored-by: Lachlan Kermode <lachiekermode@gmail.com>

--- acorn-sys/gen_bindings.sh
+++ acorn-sys/gen_bindings.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# Generate Rust bindings to the Faiss C API
+# Generate Rust bindings to the ACORN C API
 #
 # Ensure that the submodule is updated and checked out in the intended revision
 if ! which bindgen > /dev/null; then
@@ -8,17 +8,17 @@ if ! which bindgen > /dev/null; then
     exit 1
 fi
 
-repo_url=https://github.com/facebookresearch/faiss
-repo_rev=v1.8.0
+repo_url=https://github.com/csirianni/ACORN.git 
+repo_branch=oak
 cuda_root=/opt/cuda
 
-if [ ! -d faiss ]; then
-    git clone "$repo_url" faiss --branch "$repo_rev" --depth 1
+if [ ! -d acorn ]; then
+    git clone "$repo_url" acorn --branch "$repo_branch" --depth 1
 fi
 
 bindgen_opt='--allowlist-function faiss_.* --allowlist-type idx_t|Faiss.* --opaque-type FILE'
 
-headers=`ls faiss/c_api/*_c.h faiss/c_api/impl/*_c.h faiss/c_api/utils/*_c.h`
+headers=`ls /acorn/c_api/*_c.h /acorn/c_api/impl/*_c.h /acorn/c_api/utils/*_c.h`
 echo '// Auto-generated, do not edit!' > c_api.h
 for header in $headers; do
     echo "#include \""$header"\"" >> c_api.h;
@@ -28,14 +28,6 @@ cmd="bindgen --rust-target 1.59 $bindgen_opt c_api.h -o src/bindings.rs"
 echo ${cmd}
 ${cmd}
 
-headers=faiss/c_api/gpu/*_c.h
-for header in $headers; do
-    echo "#include \""$header"\"" >> c_api.h;
-done
-
-cmd="bindgen --rust-target 1.59 $bindgen_opt c_api.h -o src/bindings_gpu.rs -- -Ifaiss/c_api -I$cuda_root/include"
-echo ${cmd}
-${cmd}
-
 # clean up
 rm -f c_api.h
+
